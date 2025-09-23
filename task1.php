2e1ec03edc88506c536714bd3e0781c8cc77bd05
Дмитрий Чуринов <41968885+DmitriiChurinov@users.noreply.github.com>
2025-09-23 10:25:22 +0300

    task1.php
    Упражнение 1: Написать скрипт, который принимает имя и возраст через форму ($_GET), выводит приветствие и определяет, совершеннолетний ли человек.

---
+++ task1.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="ru">
+<head>
+    <meta charset="UTF-8">
+    <title>Приветствие</title>
+</head>
+<body>
+    <form method="get" action="">
+        <label for="name">Имя:</label>
+        <input type="text" id="name" name="name" required>
+        <br><br>
+        <label for="age">Возраст:</label>
+        <input type="number" id="age" name="age" required>
+        <br><br>
+        <input type="submit" value="Отправить">
+    </form>
+
+    <?php
+    if (isset($_GET['name']) && isset($_GET['age'])) {
+        $name = htmlspecialchars($_GET['name']);
+        $age = (int)$_GET['age'];
+
+        echo "<p>Привет, $name! Тебе $age лет.</p>";
+
+        if ($age >= 18) {
+            echo "<p>Ты совершеннолетний.</p>";
+        } else {
+            echo "<p>Ты несовершеннолетний.</p>";
+        }
+    }
+    ?>
+</body>
+</html>
